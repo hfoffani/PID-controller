9da70565d436307fb2506911e4410ea2b558937f
hfoffani <hfoffani@gmail.com>
2017-07-27 12:00:29 +0200

    Initialization and steering angle. ~dev

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,6 +11,13 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Kd = Kd;
+    PID::Ki = Ki;
+
+    p_error = 0.0;
+    d_error = 0.0;
+    i_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,11 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  double Kp = 0.0;
+  double Ki = 0.0;
+  double Kd = 0.0;
+  pid = PID();
+  pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,6 +62,8 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+          pid.UpdateError(cte);
+          steer_value = - pid.Kp * pid.p_error - pid.Kd * pid.d_error - pid.Ki * pid.i_error;
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
