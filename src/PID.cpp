5dbaac2588f10b4510880076c88295d0f9c5eb74
hfoffani <hfoffani@gmail.com>
2017-07-27 12:22:10 +0200

    Implement PID.

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,16 +12,21 @@ PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
     PID::Kp = Kp;
-    PID::Kd = Kd;
     PID::Ki = Ki;
+    PID::Kd = Kd;
 
     p_error = 0.0;
-    d_error = 0.0;
     i_error = 0.0;
+    d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
 }
+
 
 double PID::TotalError() {
 }
