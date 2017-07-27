e081f2a871bf7c4e5c9878fe7a9679874bf9a8cd
hfoffani <hfoffani@gmail.com>
2017-07-27 19:45:55 +0200

    Shut up warning. ~dev

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,7 @@ int main()
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+          // double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
           /*
           * : Calcuate steering value here, remember the steering value is
